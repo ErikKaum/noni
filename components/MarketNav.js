4d737aec9899cb439bb254fabbde206212487c69
ErikKaum <erik.kaum@gmail.com>
2022-06-26 12:29:53 +0300

    basic marketplace functionality added

---
+++ components/MarketNav.js
@@ -0,0 +1,121 @@
+import Link from "next/link"
+
+import { ethers } from "ethers"
+import { useContext, useEffect, useState } from "react"
+import { Toaster, toast } from "react-hot-toast"
+
+import { userContext } from "../lib/context"
+
+import { CONTRACT_ADDRESS_NONI } from "../lib/constants";
+import Noni from "../lib/contracts/Noni.json"
+
+const MarketNav = () => {
+
+  const [noniCount, setNoniCount] = useState(0)
+  const { account, setAccount } = useContext(userContext)
+
+  const connectWallet = async () => {
+    try {
+      const { ethereum } = window
+      if (ethereum) {
+        const provider = new ethers.providers.Web3Provider(ethereum)
+        const accounts = await provider.send("eth_requestAccounts", []);
+        setAccount(accounts[0])
+        console.log('connected to:', accounts[0])
+
+        const chain = ethereum.chainId
+        if (chain !== "0x4") {
+
+          toast('and then reload the page!', {
+            icon: '🔃',
+          });
+
+          toast('Looks like you are not on Rinkeby! Switch please.', {
+            icon: '👀',
+          });
+          
+        }
+      }   
+
+    } catch(e) {
+      console.log(e)
+      toast.error('No metamask found 🤷')
+    }
+  }
+
+  const mintNoni = async() => {
+    const { ethereum } = window
+
+    const provider = new ethers.providers.Web3Provider(ethereum);
+    const signer = provider.getSigner();
+    const contract = new ethers.Contract(CONTRACT_ADDRESS_NONI, Noni.abi, signer);
+
+    // For now just use the default json cid
+    const cid = "QmNQ1ABiGJ9fv9wzxf1k1eKbc8nvXBQw7VnQSNedeRwwfo"
+    console.log(cid)
+    await contract.safeMint(cid)
+  }
+
+  useEffect(() => {
+    const getNoniCount = async() => {
+      const { ethereum } = window
+
+      const provider = new ethers.providers.Web3Provider(ethereum);
+      const signer = provider.getSigner();
+      const contract = new ethers.Contract(CONTRACT_ADDRESS_NONI, Noni.abi, signer);
+
+      const noniCount = await contract.getNumberOfNonis()
+      setNoniCount(noniCount.toNumber()+1)
+    }
+    getNoniCount()
+  },[])
+  
+  const Text = () => {
+    if (account) {
+      return(
+        <p> 🟢 Connect wallet</p>
+      ) 
+    } else {
+    return(
+      <p> 🔴 Connect wallet</p>
+    )
+    }
+  }
+
+
+  return(
+    
+    <div className="fixed flex flex-col w-1/4 h-screen border-r-2 border-black justify-around items-center">
+    <Toaster/>
+    
+    <div className="flex flex-col space-y-10 w-3/5 p-5 border border-white">
+
+      <h2 className="text-center font-bold text-2xl">THE MINT MACHINE</h2>
+      <div className="flex flex-col">
+        <p className="text-center font-medium text-base">{noniCount} Nonis Minted</p>
+        <p className="text-center font-medium text-base">Go get yours</p>
+      </div>
+      <button onClick={mintNoni} className="border-2 border-black w-full text-lg font-medium py-2 hover:bg-noni-lb bg-noni-pink ">
+        MINT 
+      </button>
+  
+    </div>
+
+    <div className="flex flex-col w-3/5 space-y-5">
+      <button onClick={connectWallet} className="border-2 border-black w-full text-lg font-medium py-2 bg-noni-lb hover:bg-noni-pink">
+        <Text/>
+      </button>
+      <div>
+        <Link href={"/app"} passHref>
+          <button className="border-2 border-black w-full text-lg font-medium py-2 bg-noni-lb hover:bg-noni-pink">
+            Back to App
+          </button>   
+        </Link> 
+      </div>
+    </div>
+
+  </div>
+  )
+}
+
+export default MarketNav
--- pages/market-place.js
+++ pages/market-place.js
@@ -1,11 +1,99 @@
+import Link from "next/link"
+
+import MarketNav from "../components/MarketNav"
+import { useEffect, useState, useContext } from "react"
+import { userContext, agentContext } from "../lib/context"
 
 
-const marketPlace = () => {
-    return(
-        <div className="w-full bg-noni-blue h-screen flex flex-col justify-center items-center">
-            <h1 className="text-center text-white text-6xl font-bold">Coming soon!</h1>
+const MainMarket = () => {
+  const [nonis, setNonis] = useState([])
+  
+  useEffect(() => {
+    const getNonis = async () => {
+        
+        const noni = {
+          tokenId: 0,
+          name: "0xNoni0",
+          elo : 400,
+          image: "imageURI",
+          contentID: "contentID",
+        }
+        const noniArray = [noni, noni, noni, noni, noni]
+        setNonis(noniArray)
+      }
+  
+      getNonis()
+  
+  },[])
+
+  return(
+    <div className="w-3/4 ml-[calc(25%)] h-full flex flex-col justify-center items-center">
+      
+      <div className="flex w-full my-24 items-center justify-center">
+        <h1 className="text-4xl font-bold">MARKET PLACE</h1>
+      </div>
+      
+      <div className="flex mb-10">
+        <p className="text-xl font-medium">The place to buy and sell Nonis</p>
+      </div>
+
+      <div className="flex flex-col">
+
+        <div className="flex w-full h-6/5 justify-center">
+        <div className="grid grid-cols-2 lg:grid-cols-3 gap-10">
+          
+          {nonis.map((noni) => {
+
+            return(
+            <div key={noni.name} className="flex flex-col">
+              <div className="flex w-56 h-56 border-2 border-black p-2 mb-2 bg-slate-100">
+                {/* <img src={noni.image} alt={noni.name} /> */}
+              </div>
+
+              <div className="flex flex-col mb-5">
+                <p className="text-center font-medium">{noni.name}</p>
+                <p>Price: 10ETH </p>
+                <p>ELO score: {noni.elo}</p>
+              </div>
+
+              <div className="flex justify-between">
+                <button className="border border-black-2 hover:opacity-60">
+                  Buy
+                </button>
+
+                <button onClick={() => setAgent(noni)} className="border border-black-2 hover:opacity-60">
+                  Activate
+                </button>
+ 
+              </div> 
+  
+            </div>
+            )
+          })}
+
         </div>
+      </div>
+
+    </div>
+    </div>
+
     )
 }
 
-export default marketPlace
+const MarketPlace = () => {
+    const { account, setAccount } = useContext(userContext)
+
+    return (
+        <div className="w-full min-h-screen h-full bg-noni-blue">
+          <MarketNav />
+          {account && <MainMarket/>}
+          {!account &&
+          <div className="w-3/4 ml-[calc(25%)] h-screen flex justify-center items-center">
+            <h1 className="text-4xl font-bold">Wallet is not connected 🤷</h1>
+          </div>} 
+        </div>
+      )}
+
+export default MarketPlace
+
+
