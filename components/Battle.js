3fbdfd47c53dcb07cf19bb1cf4c9a3c2309e8f55
ErikKaum <erik.kaum@gmail.com>
2022-07-08 20:09:08 +0300

    battle onchain

--- components/Battle.js
+++ components/Battle.js
@@ -1,39 +1,99 @@
 import { ethers } from "ethers";
 
-import { useContext, useState } from "react"
+import { useContext, useEffect, useState } from "react"
 import { userContext, agentContext } from "../lib/context"
 
 import GameAbi from "../lib/contracts/Game.json"
 import { CONTRACT_ADDRESS_GAME } from "../lib/constants";
 
-import Game from "./game/Game"
+import BattleGame from "./game/BattleGame"
+import toast from "react-hot-toast";
 
 
 const Battle = () => {
   const { agent, setAgent } = useContext(agentContext)
   const { account, setAccount } = useContext(userContext)
   const [start, setStart] = useState(false)
+  const [allGames, setAllGames] = useState([])
+  const [activeGame, setActiveGame] = useState()
+  const [player2, setPlayer2] = useState(null)
+  
+  useEffect(() => {
+    const getChallenges = async() => {
+      const { ethereum } = window
 
-  const init = async() => {
-    const { ethereum } = window
+      const provider = new ethers.providers.Web3Provider(ethereum);
+      const signer = provider.getSigner();
+      const contract = new ethers.Contract(CONTRACT_ADDRESS_GAME, GameAbi.abi, signer);
+      const incomingChallenges =  await contract.getMyChallenges()
+      const createdChallenges = await contract.getMyGames()
+      // let createdChallenges = []
+    
+      const currentGames = incomingChallenges.concat(createdChallenges) 
+      let tempGames = [] 
 
-    const provider = new ethers.providers.Web3Provider(ethereum);
-    const signer = provider.getSigner();
-    const contract = new ethers.Contract(CONTRACT_ADDRESS_GAME, GameAbi.abi, signer);
-    // await contract.initGame("0x70997970C51812dc3A010C7d01b50e0d17dc79C8")
-    const game = await contract.getGame(1)
-    console.log(game)
+      for (const gameId of currentGames) {
+        const game = await contract.getGame(gameId)
+        
+        let verified 
+        game.players.forEach((item, idx) => {
+          if (item === account) {
+            if (game.verification[idx] === true) {
+             verified = true 
+            } else {
+              verified = false
+            }
+          }
+        })
+        
+        const tempGame = {
+          gameId: gameId.toNumber(),
+          game : game.game,
+          players : game.players,
+          state : game.state,
+          turn : game.turn,
+          verification : game.verification,
+          userVerified : verified
+        }
+        tempGames.push(tempGame)
+      }
+      setAllGames(tempGames)
+    }
+    getChallenges()
+  },[])
+
+  const activateGame = async (gameId) => {
+    allGames.forEach((game) => {
+      if (gameId == game.gameId) {
+        setActiveGame(game)
+      }
+    })
   }
 
-  const verify = async() => {
+  const challenge = async() => {
+    const address = ethers.utils.isAddress(player2)
+    if (address === true) {
+      const { ethereum } = window
+      const provider = new ethers.providers.Web3Provider(ethereum);
+      const signer = provider.getSigner();
+      const contract = new ethers.Contract(CONTRACT_ADDRESS_GAME, GameAbi.abi, signer);
+      await contract.initGame(player2)
+    } else {
+      toast('Not vaild address', {
+        icon: '🤔',
+      });
+    }
+  }
+
+  const verify = async(gameId) => {
     const { ethereum } = window
 
     const provider = new ethers.providers.Web3Provider(ethereum);
     const signer = provider.getSigner();
     const contract = new ethers.Contract(CONTRACT_ADDRESS_GAME, GameAbi.abi, signer);
-    
-    const lol = await contract.verifyGame(0)
-    console.log(lol)
+
+    // this will need the noni in the future 
+    await contract.verifyGame(gameId)
   }
 
   const move = async() => {
@@ -46,7 +106,6 @@ const Battle = () => {
     const contract = new ethers.Contract(CONTRACT_ADDRESS_GAME, GameAbi.abi, signer);
     
     const lol = await contract.makeMove(8, 0)
-    console.log(lol)
   }
 
   const checkWinner = async() => {
@@ -57,7 +116,6 @@ const Battle = () => {
     const contract = new ethers.Contract(CONTRACT_ADDRESS_GAME, GameAbi.abi, signer);
     
     const lol = await contract.getMyChallenges()
-    console.log(lol)
   }
 
 
@@ -68,22 +126,74 @@ const Battle = () => {
         </div>
 
         {agent &&
-        <div className="flex w-full justify-center space-x-5">
-          {/* <Game start={start} setStart={setStart} currentModel={agent.model}/> */}
-          <div className="flex flex-col w-1/3 items-center justify-around">
+        <div className="flex w-full justify-center space-x-10">
 
-              <div className="flex flex-col items-center"> 
-                <h2 className="text-xl font-semibold">GAME stats</h2>
+          <div className="flex flex-col space-y-5"> 
+            <div className="flex flex-col items-center">
+              
+              {activeGame &&  
+              <>
+                <h2 className="text-xl font-semibold">GAME {activeGame.gameId}</h2>
                 <p>Your are Battling with: {agent.name}</p>
-              </div>
+                <p>against: {agent.name}</p>
+                <p>Game is: {activeGame.state}</p>
+              </>
+              }
+              
+              {!activeGame &&  
+              <>
+                <h2 className="text-xl font-semibold">No game activated</h2>
+              </>
+              } 
+            
+            </div>
+
+            <BattleGame activeGame={activeGame} start={start} setStart={setStart} />
+          </div>
+
+          <div className="flex flex-col w-1/3 items-center justify-around">
 
               <div className="flex flex-col items-center">
                 <h2 className="text-xl font-semibold">How to play?</h2>
                 <p>It&apos;s Tic Tac Toe not rocket science 🚀</p>
-                <p>Hit the button below and start playing!</p>  
+                <p>Either challenge someone from the button below</p>
+                <p className="mb-5">or accept a challenge from the list</p>
+                
+                <button onClick={challenge} className="border-2 border-black w-2/3 text-lg font-medium py-2 mb-2 bg-noni-lb hover:bg-noni-pink">
+                  CREATE CHALLENGE
+                </button>
+                <input onChange={e => setPlayer2(e.target.value)} type="text" placeholder="Your friends wallet address" className="text-black p-1 w-2/3"></input>
+
               </div>
-            
-            <button onClick={() => setStart(true)} className="border-2 border-black w-2/3 text-lg font-medium py-2 bg-noni-lb hover:bg-noni-pink">START GAME</button>
+
+              <div className="flex flex-col w-full items-center py-4">
+                {allGames && allGames.length > 0 && 
+                <h2 className="text-xl font-semibold mb-2">Your current games!</h2>
+                }
+                {allGames && allGames.length === 0 &&
+                <h2 className="text-xl font-semibold mb-2">You have no active games, go challenge someone!</h2>
+                }
+                {allGames && allGames.map((game) => {
+                  return(
+                    <div key={game.gameId} className="flex w-full items-center justify-between">
+                      <p>Game {game.gameId}</p>
+                      <div className="flex w-1/2 items-center justify-between">
+                        <button onClick={() => activateGame(game.gameId)} className="border border-white mr-2 mb-2 p-1">Activate</button>
+
+                        <div className="flex w-full items-center justify-center">
+                        {!game.userVerified && 
+                          <button onClick={() => verify(game.gameId)} className="border border-white p-1 mb-2">Accept challenge</button>
+                        }
+
+                        {game.userVerified && 
+                          <p className="border border-white p-1 text-center mb-2">Challenge accepted</p>
+                        }
+                        </div>
+                        </div>
+                    </div>
+                  )
+                })}
+              </div>
             
             </div>
         </div>
@@ -95,10 +205,6 @@ const Battle = () => {
           <p className="text-4xl">👈</p>
         </div>
         }
-
-        <button onClick={init}>init</button>
-        <button onClick={verify}> verify</button>
-        <button onClick={checkWinner}> move</button>
 
       </div>
 
--- components/game/BattleGame.js
+++ components/game/BattleGame.js
@@ -1,5 +1,5 @@
 import { useContext, useState, useEffect } from "react"
-
+import { ethers } from "ethers"
 import Square from "./Square"
 import { Patterns } from "./Patterns"
 
@@ -7,15 +7,19 @@ import toast from "react-hot-toast"
 
 import { tensor } from "@tensorflow/tfjs"
 
+import GameAbi from "../../lib/contracts/Game.json"
+import { CONTRACT_ADDRESS_GAME } from "../../lib/constants";
+
 import { userContext, agentContext } from "../../lib/context"
 
-const Game = ({ start, setStart, overrideBoard }) => {
+const  BattleGame = ({ activeGame, start, setStart }) => {
 
   const { agent, setAgent } = useContext(agentContext)
+  const { account, setAccount } = useContext(userContext) 
   const [board, setBoard] = useState(["", "", "", "", "", "", "", "", ""])
   const [player, setPlayer] = useState("O")
   const [result, setResult] = useState({winner : "none", state: "none"})
-  
+
   const getBoardAsTensor = () => {
     let inputArray = []
     board.forEach((value) => {
@@ -23,7 +27,7 @@ const Game = ({ start, setStart, overrideBoard }) => {
             inputArray.push(1)
         }
         else if (value === "O") {
-          inputArray.push(-1) 
+          inputArray.push(-1)
         }
         else {
             inputArray.push(0)
@@ -90,11 +94,11 @@ const Game = ({ start, setStart, overrideBoard }) => {
     }
   }
 
-  const chooseSquare = (square) => {
+  const chooseSquare = (square, mark) => {
     setBoard(
         board.map((value, index) => {
             if (index === square && value === "") {
-              return player
+              return mark
             }
             return value
         })
@@ -106,29 +110,6 @@ const Game = ({ start, setStart, overrideBoard }) => {
     setPlayer("O")
     setStart(false)
   }
-
-  useEffect(() => {
-
-    if (player === "X" && agent.model) {
-    const inputTensor = getBoardAsTensor()
-    let preds = agent.model.predict(inputTensor);
-    preds = preds.reshape([-1])
-
-    const argPreds = argSort(preds.arraySync())
-
-    let choice
-    for (let i = 0; i < argPreds.length; i++) {
-      choice = argPreds[i]
-      const legal = checkIfLegal(choice)
-      
-      if (legal === true) {
-        break
-      }
-    }
-    chooseSquare(choice)
-    }
-  
-  },[player])
 
   useEffect(() => {
     if (start === true) {
@@ -163,9 +144,9 @@ const Game = ({ start, setStart, overrideBoard }) => {
   }, [result])
 
   useEffect(() => {
-    if (overrideBoard !== undefined) {
+    if (activeGame !== undefined) {
       let newBoard = []
-      overrideBoard.forEach((item) => {
+      activeGame.game.forEach((item) => {
         if (item === 0) {
           newBoard.push("")
         }
@@ -177,39 +158,72 @@ const Game = ({ start, setStart, overrideBoard }) => {
         }
       })
       setBoard(newBoard)
-      console.log(newBoard)
     }
-  }, [setBoard, overrideBoard])
- 
+  }, [setBoard, activeGame])
+
   const makeMove = async() => {
-    setStart(true)
+    if (agent.model) {
+        const inputTensor = getBoardAsTensor()
+        let preds = agent.model.predict(inputTensor);
+        preds = preds.reshape([-1])
+
+        const argPreds = argSort(preds.arraySync())
+
+        let choice
+        for (let i = 0; i < argPreds.length; i++) {
+            choice = argPreds[i]
+            const legal = checkIfLegal(choice)
+
+            if (legal === true) {
+                break
+            }
+        }
+
+        const { ethereum } = window
+        const provider = new ethers.providers.Web3Provider(ethereum);
+        const signer = provider.getSigner();
+        const contract = new ethers.Contract(CONTRACT_ADDRESS_GAME, GameAbi.abi, signer);
+        
+        let mark
+        if (activeGame.players[0] === account) {
+            mark = 'X'
+        } else {
+            mark = 'O'
+        }
+        
+        await contract.makeMove(choice, activeGame.gameId)
+        chooseSquare(choice, mark)
+        }
   }
 
   return(
-    <div className="flex flex-col w-96 h-96 space-y-2">
-        <div className="flex h-1/3 space-x-2">
-          <Square value={board[0]} chooseSquare={() => chooseSquare(0)} start={start}/>
-          <Square value={board[1]} chooseSquare={() => chooseSquare(1)} start={start}/>
-          <Square value={board[2]} chooseSquare={() => chooseSquare(2)} start={start}/>
+    <div className="flex flex-col justify-center items-center">
+        <div className="flex flex-col w-96 h-96 space-y-2 mb-5">
+            <div className="flex h-1/3 space-x-2">
+            <Square value={board[0]} chooseSquare={() => chooseSquare(0)} start={start}/>
+            <Square value={board[1]} chooseSquare={() => chooseSquare(1)} start={start}/>
+            <Square value={board[2]} chooseSquare={() => chooseSquare(2)} start={start}/>
+            </div>
+
+            <div className="flex h-1/3 space-x-2">
+            <Square value={board[3]} chooseSquare={() => chooseSquare(3)} start={start}/>
+            <Square value={board[4]} chooseSquare={() => chooseSquare(4)} start={start}/>
+            <Square value={board[5]} chooseSquare={() => chooseSquare(5)} start={start}/>
+            </div>
+
+            <div className="flex h-1/3 space-x-2">
+            <Square value={board[6]} chooseSquare={() => chooseSquare(6)} start={start}/>
+            <Square value={board[7]} chooseSquare={() => chooseSquare(7)} start={start}/>
+            <Square value={board[8]} chooseSquare={() => chooseSquare(8)} start={start}/>
+            </div>
         </div>
-
-        <div className="flex h-1/3 space-x-2">
-          <Square value={board[3]} chooseSquare={() => chooseSquare(3)} start={start}/>
-          <Square value={board[4]} chooseSquare={() => chooseSquare(4)} start={start}/>
-          <Square value={board[5]} chooseSquare={() => chooseSquare(5)} start={start}/>    
-        </div>
-
-        <div className="flex h-1/3 space-x-2">
-          <Square value={board[6]} chooseSquare={() => chooseSquare(6)} start={start}/>
-          <Square value={board[7]} chooseSquare={() => chooseSquare(7)} start={start}/>
-          <Square value={board[8]} chooseSquare={() => chooseSquare(8)} start={start}/>
-        </div>
-
-        <button onClick={makeMove}>LOL</button>
-
+        
+        {activeGame && activeGame.game.length > 0 &&
+        <button onClick={makeMove} className="border-2 border-black w-2/3 text-lg font-medium py-2 bg-noni-lb hover:bg-noni-pink" >MAKE MOVE</button>
+        }
+    
     </div>
-
   )
 }
 
-export default Game
+export default BattleGame
